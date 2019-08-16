07e180641db226ca6b450613263277850654a252
autumn-ragland <raglanda998@gmail.com>
2019-08-16 14:14:47 -0500

    Graded 08-16-2019 14:13:44

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # javascript-class-review1-cw
-
+0/0 correct. Partial credit on #1. See index.js for comments. Score:1 point
 ### Problem 1:
 
 Put the start of your program in a main function. Create a class for Books with name, rating, genre, and author. Create 3 instances of Books and put them in an array. Afterwards print each one in the format below:
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// you never defined a main function
+
 function bookFunct()
 {
    console.log(bookArray)
@@ -17,8 +19,9 @@ var  fight= new Books("Fight Club","R", "adventure", "Brad");
 var ramp= new Books("Rampage", "PG-13","action","Rock");
 var books =[];
 
-function (){
-    jungle.push(books);
-fight.push(books);
+function (){ // this is an anoynomous function and it should be named and called
+    jungle.push(books); // incorrect syntax it SHOULD be arrayName.push(itemToPush)
+fight.push(books); // you don't NEED to push the book objects into your Books array in it's own function. You can just do it in the main function
 ramp.push(books);
- books.forEach(bookFunct)}
+ books.forEach(bookFunct)// I'm assuming this is incomplete otherwise watch you syntax 
+}
